0d7915c7c6b7d2f138b7e9628c6dfb9e39ba3730
qrmoon <cz_piotr@protonmail.com>
2021-04-21 12:25:24 +0200

    Revert "Remove local liblua5.2"
    This reverts commit 60f7b021d6b683478599e36898486d9801c8a3b4.

--- src/api/api.h
+++ src/api/api.h
@@ -1,9 +1,9 @@
 #ifndef API_H
 #define API_H
 
-#include <lua5.2/lua.h>
-#include <lua5.2/lauxlib.h>
-#include <lua5.2/lualib.h>
+#include "lib/lua52/lua.h"
+#include "lib/lua52/lauxlib.h"
+#include "lib/lua52/lualib.h"
 
 #define API_TYPE_FONT "Font"
 
